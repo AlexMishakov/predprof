933518c80a885c4384837c93ff6f03f34c5e7ffc
Alex Mishakov <mishakovav@s2200.ru>
2024-02-13 15:36:27 +0300

    complete task3

--- task3.py
+++ task3.py
@@ -1,16 +1,27 @@
+def search(_id_project: int, file_name: str) -> str:
+    """Фукнция поиска проекта по id."""
+    f = open(file_name)
+    f.readline()
 
-def search(_id_project: int):
-    print(f'test {_id_project}')
+    for line in f.readlines():
+        data = line.split(',')
+        if _id_project == int(data[2]):
+            f.close()
+            return f'Проект № {_id_project} делал: {data[1]} он(а) получил(а) оценку - {data[4]}'
+
+    f.close()
+    return 'Ничего не найдено'
 
 
-def main():
+def menu():
+    """Меню программы."""
     while True:
         id_project = input('Введите id проекта: ')
         if id_project == 'СТОП':
             break
 
-        search(int(id_project))
+        print(search(int(id_project), 'students.csv'))
 
 
 if __name__ == '__main__':
-    main()
+    menu()
