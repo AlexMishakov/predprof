dc62c5aac537bfd8c548e3f408d444b9ce54c525
Александр Мишаков <sasha.mishakov@gmail.com>
2024-02-13 19:12:29 +0300

    complete task2

--- task2.py
+++ task2.py
@@ -1,35 +1,38 @@
-import time
-
-
-def paste_sort(data: [int]) -> [int]:
-    """Сортировка вставками.
-    :return: Отсортированный массив
-    :param data: Массив который надо отсортировать
-    """
-    for i in range(len(data)):
-        x = data[i]
-        j = i
-        while j > 0 and x < data[j-1]:
-            data[j] = data[j-1]
-            j -= 1
-        data[j] = x
-    return data
-
-
 def main():
-    # Дописать решение задачи №2
-    pass
+    """Основная функция."""
+
+    # Подготовка данных
+    file = open('students.csv')
+    file.readline()
+    data_file = []
+    for line in file.readlines():
+        line = line.replace('\n', '')
+        data = line.split(',')
+        if data[4] == 'None':
+            data[4] = 0
+        else:
+            data[4] = int(data[4])
+        data_file.append(tuple(data))
+
+    # Сортировка вставкой
+    for i in range(len(data_file)):
+        x = data_file[i]
+        j = i
+        while j > 0 and x[4] > data_file[j-1][4]:
+            data_file[j] = data_file[j-1]
+            j -= 1
+        data_file[j] = x
+
+
+    # Вывод первых трех из 10-x классов
+    count = 0
+    for l in data_file:
+        if '10' in l[3]:
+            print(f'{count+1} место: {l[1]}')
+            count += 1
+        if count == 3:
+            break
 
 
 if __name__ == '__main__':
-    # main()
-    example_data = [4, 7, 2, 8, 4, 2, 9, 0, 1]
-    new_data = paste_sort(example_data)
-
-    start_time = time.time()
-    print(new_data)
-    print(time.time() - start_time)
-
-    start_time = time.time()
-    print(sorted(example_data))
-    print(time.time() - start_time)
+    main()
